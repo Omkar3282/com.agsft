a45b1dd21f81fa698e1cadc8bfdd8b0bdd5ff106
omkar <omkart@agsft.com>
2023-06-20 15:43:37 +0530

    Initial commit

---
+++ src/main/java/com/agsft/publish/demopublish/DemoPublishApplication.java
@@ -0,0 +1,13 @@
+package com.agsft.publish.demopublish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoPublishApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoPublishApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/agsft/publish/demopublish/DemoPublishApplicationTests.java
@@ -0,0 +1,13 @@
+package com.agsft.publish.demopublish;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoPublishApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
